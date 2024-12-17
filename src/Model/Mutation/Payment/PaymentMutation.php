liborplucnar/shopsys

[SSP-2840] The e-shop will not allow you to repeat the payment if the current payment is in the verification process.

packages/frontend-api/src/Model/Mutation/Payment/PaymentMutation.php
@@ -13,6 +13,7 @@ use Shopsys\FrameworkBundle\Model\Payment\Service\PaymentServiceFacade;
 use Shopsys\FrontendApiBundle\Model\Mutation\AbstractMutation;
 use Shopsys\FrontendApiBundle\Model\Mutation\Payment\Exception\MaxTransactionCountReachedUserError;
 use Shopsys\FrontendApiBundle\Model\Mutation\Payment\Exception\OrderAlreadyPaidUserError;
+use Shopsys\FrontendApiBundle\Model\Mutation\Payment\Exception\OrderWaitingForProcessPaymentUserError;
 use Shopsys\FrontendApiBundle\Model\Order\OrderApiFacade;
 use Throwable;
 
@@ -41,6 +42,10 @@ class PaymentMutation extends AbstractMutation
 
         if ($order->isPaid()) {
             throw new OrderAlreadyPaidUserError('Order is already paid');
+        }
+
+        if ($order->isPaymentInProcess()) {
+            throw new OrderWaitingForProcessPaymentUserError('Order is awaiting payment verification.');
         }
 
         if ($order->isMaxTransactionCountReached()) {

shopsys/frontend-api

src/Model/Mutation/Payment/PaymentMutation.php
@@ -13,6 +13,7 @@ use Shopsys\FrameworkBundle\Model\Payment\Service\PaymentServiceFacade;
 use Shopsys\FrontendApiBundle\Model\Mutation\AbstractMutation;
 use Shopsys\FrontendApiBundle\Model\Mutation\Payment\Exception\MaxTransactionCountReachedUserError;
 use Shopsys\FrontendApiBundle\Model\Mutation\Payment\Exception\OrderAlreadyPaidUserError;
+use Shopsys\FrontendApiBundle\Model\Mutation\Payment\Exception\OrderWaitingForProcessPaymentUserError;
 use Shopsys\FrontendApiBundle\Model\Order\OrderApiFacade;
 use Throwable;
 
@@ -41,6 +42,10 @@ class PaymentMutation extends AbstractMutation
 
         if ($order->isPaid()) {
             throw new OrderAlreadyPaidUserError('Order is already paid');
+        }
+
+        if ($order->hasPaymentInProcess()) {
+            throw new OrderWaitingForProcessPaymentUserError('Order is awaiting payment verification.');
         }
 
         if ($order->isMaxTransactionCountReached()) {